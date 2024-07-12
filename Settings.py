68f2eabbc4c90fdb0ef6f272e1c200ea90eb1831
alexmj003 <idkyflymf@gmail.com>
2024-07-12 15:58:48 -0500

    enabled multiple settings files

--- Settings.py
+++ Settings.py
@@ -1,10 +1,13 @@
 import json
-default_json = 'settings.json'
+default_json = 'default_settings.json'
 
 class SettingsJson():
     def __init__(self, json_file_path = default_json):
         with open(json_file_path) as json_file_path:
             self.json_file = json.load(json_file_path)
+        self.json_file_path = self.json_file['current settings']
+        with open(self.json_file_path) as json_file:
+            self.json_file = json.load(json_file)
         self.automatic_csv_export = self.json_file['automatic csv export']
         self.automatic_prediction_clear_data = self.json_file['automatic prediction clear data']
         self.clear_data_on_clear_images = self.json_file['clear data on clear images']
