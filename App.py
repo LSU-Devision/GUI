5e0a6086392e4420a6f5a6acbab4d3d9f537d168
Skylar Wilson <136635193+skylarwilson@users.noreply.github.com>
2024-07-15 20:59:53 -0500

    set minimum size and adjusted grid

--- App.py
+++ App.py
@@ -1,17 +1,25 @@
-from MainFrame import MainFrame
 import tkinter as tk
+from MainFrame import MainFrame
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-
         self.title('Devision')
-        self.geometry('1000x1000')
+        #self.geometry('1000x1000')
+        self.configure(bg="#12EFE4")
+
+        # set minimum window size
+        self.minsize(750, 850)
+        
+        # Use grid layout for the main window
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
         self.main = MainFrame(self)
-        self.main.pack(pady=5)
+        self.main.grid(row=0, column=0, sticky="nsew")
 
 if __name__ == "__main__":
-    # run the application
+    # Run the application
     app = App()
-    # start the event loop
+    # Start the event loop
     app.mainloop()
