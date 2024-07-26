25eab956fa95dbc51b2d8fc77fa505467ccd5e5d
alexmj003 <idkyflymf@gmail.com>
2024-07-26 10:23:05 -0500

    Created excel tabs

--- ExcelWindow.py
+++ ExcelWindow.py
@@ -43,72 +43,86 @@ class ExcelWindow(tk.Toplevel):
         y = main_window_height // 2 - pop_up_window_height // 2  # center the pop-up window vertically
         # set the title of the pop up window
         self.title('Excel Options')
+
+
         # set the geometry of the pop up window
         self.geometry(f'{pop_up_window_width}x{pop_up_window_height}+{x}+{y}')
         # checks to see if the excel file is used, creates a substring if so
         if self.excel_editor.get_excel_file() is not None:
             self.excel_editor.get_substring()
+        # convert frame into notebook
+        self.notebook=ttk.Notebook(self)
+        # add the notebook using the grid method
+        self.notebook.grid(row=0,column=0)
+        # create the first tab
+        self.tab1=ttk.Frame(self.notebook)
+        # create the second tab
+        self.tab2=ttk.Frame(self.notebook)
+        # add the Basic tab
+        self.notebook.add(self.tab1,text="Basic")
+        # add the Advanced tab
+        self.notebook.add(self.tab2,text="Advanced")
         # creates the page
         self.run()
     def create_page(self):
         # create the export excel button
-        self.export_excel_button = ttk.Button(self, text='Export Excel', command=self.master.excel_editor.export_predictions_to_excel)
+        self.export_excel_button = ttk.Button(self.tab1, text='Export Excel', command=self.master.excel_editor.export_predictions_to_excel)
         # create the load excel by selection button
-        self.load_excel_by_selection_button = ttk.Button(self, text='Load Excel', command=self.load_excel_by_selection)
+        self.load_excel_by_selection_button = ttk.Button(self.tab1, text='Load Excel', command=self.load_excel_by_selection)
         # create the clear predictions button
-        self.clear_prediction_data_button = ttk.Button(self, text='Clear Predictions',command=self.master.clear_predicted_data)
+        self.clear_prediction_data_button = ttk.Button(self.tab1, text='Clear Predictions',command=self.master.clear_predicted_data)
         # create the clear excel file button
-        self.clear_excel_file_button = ttk.Button(self, text='Clear Excel File', command=self.clear_excel_file)
+        self.clear_excel_file_button = ttk.Button(self.tab1, text='Clear Excel File', command=self.clear_excel_file)
         # protocol for closing the window
         self.protocol("WM_DELETE_WINDOW", lambda: self.close_window())
 
         # create the excel index column
-        self.excel_index_column_toggle = ttk.Button(self, text='Excel Index Column')
+        self.excel_index_column_toggle = ttk.Button(self.tab2, text='Excel Index Column')
         # create the excel index column dropdown
-        self.excel_index_column_dropdown = ttk.Combobox(self, state='readonly',values=['None', '1', '2', '3', '4', '5'])
+        self.excel_index_column_dropdown = ttk.Combobox(self.tab2, state='readonly',values=['None', '1', '2', '3', '4', '5'])
         # set the excel index column dropdown to the current value
         self.excel_index_column_dropdown.set(self.excel_editor.get_excel_index_column_index())
         # bind the event to the excel index column dropdown
         self.excel_index_column_dropdown.bind("<<ComboboxSelected>>", lambda event: self.excel_editor.set_excel_index_column_value(self.excel_index_column_dropdown.get()))
 
         # create the excel date column
-        self.excel_date_column_toggle = ttk.Button(self, text='Excel Date Column')
+        self.excel_date_column_toggle = ttk.Button(self.tab2, text='Excel Date Column')
         # create the excel date column dropdown
-        self.excel_date_column_dropdown = ttk.Combobox(self, state='readonly',values=['None', '1', '2', '3', '4', '5'])
+        self.excel_date_column_dropdown = ttk.Combobox(self.tab2, state='readonly',values=['None', '1', '2', '3', '4', '5'])
         # set the excel date column dropdown to the current value
         self.excel_date_column_dropdown.set(self.excel_editor.get_excel_date_column_index())
         # bind the event to the excel date column dropdown
         self.excel_date_column_dropdown.bind("<<ComboboxSelected>>", lambda event: self.excel_editor.set_excel_date_column_value(self.excel_date_column_dropdown.get()))
 
         # create the excel time column
-        self.excel_time_column_toggle = ttk.Button(self, text='Excel Time Column')
+        self.excel_time_column_toggle = ttk.Button(self.tab2, text='Excel Time Column')
         # create the excel time column dropdown
-        self.excel_time_column_dropdown = ttk.Combobox(self, state='readonly',values=['None', '1', '2', '3', '4', '5'])
+        self.excel_time_column_dropdown = ttk.Combobox(self.tab2, state='readonly',values=['None', '1', '2', '3', '4', '5'])
         # set the excel time column dropdown to the current value
         self.excel_time_column_dropdown.set(self.excel_editor.get_excel_time_column_index())
         # bind the event to the excel time column dropdown
         self.excel_time_column_dropdown.bind("<<ComboboxSelected>>", lambda event: self.excel_editor.set_excel_time_column_value(self.excel_time_column_dropdown.get()))
 
         # create the excel file name column
-        self.excel_file_name_column_toggle = ttk.Button(self, text='Excel File Name Column')
+        self.excel_file_name_column_toggle = ttk.Button(self.tab2, text='Excel File Name Column')
         # create the excel file name column dropdown
-        self.excel_file_name_column_dropdown = ttk.Combobox(self, state='readonly',values=['None', '1', '2', '3', '4', '5'])
+        self.excel_file_name_column_dropdown = ttk.Combobox(self.tab2, state='readonly',values=['None', '1', '2', '3', '4', '5'])
         # set the excel file name column dropdown to the current value
         self.excel_file_name_column_dropdown.set(self.excel_editor.get_excel_file_name_column_index())
         # bind the event to the excel file name column dropdown
         self.excel_file_name_column_dropdown.bind("<<ComboboxSelected>>", lambda event: self.excel_editor.set_excel_file_name_column_value(self.excel_file_name_column_dropdown.get()))
 
         # create the excel total count column
-        self.excel_total_count_column_toggle = ttk.Button(self, text='Excel Total Count Column')
+        self.excel_total_count_column_toggle = ttk.Button(self.tab2, text='Excel Total Count Column')
         # create the excel total count column dropdown
-        self.excel_total_count_column_dropdown = ttk.Combobox(self, state='readonly',values=['None', '1', '2', '3', '4', '5'])
+        self.excel_total_count_column_dropdown = ttk.Combobox(self.tab2, state='readonly',values=['None', '1', '2', '3', '4', '5'])
         # set the excel total count column dropdown to the current value
         self.excel_total_count_column_dropdown.set(self.excel_editor.get_excel_total_count_column_index())
         # bind the event to the excel total count column dropdown
         self.excel_total_count_column_dropdown.bind("<<ComboboxSelected>>", lambda event: self.excel_editor.set_excel_total_count_column_value(self.excel_total_count_column_dropdown.get()))
 
         # create the save excel column button
-        self.save_excel_column_button = ttk.Button(self, text='Save Excel Column', command=self.excel_editor.save_excel_settings)
+        self.save_excel_column_button = ttk.Button(self.tab2, text='Save Excel Column', command=self.excel_editor.save_excel_settings)
         # bind the event to the save excel column button
         self.save_excel_column_button.bind("<Button-1>",lambda event: messagebox.showinfo("Saved", "Excel Column Saved"))
     def load_page(self):
