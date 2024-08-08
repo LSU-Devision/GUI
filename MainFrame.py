c8e13afcf747119434905ba533f953ca01a7a9ee
alexmj003 <idkyflymf@gmail.com>
2024-08-08 16:58:52 -0500

    cleaned up the mainframe, updated the clear data method. added pop up

--- MainFrame.py
+++ MainFrame.py
@@ -1,11 +1,9 @@
 import tkinter as tk
-from tkinter import ttk, filedialog,messagebox
+from tkinter import ttk, filedialog, messagebox
 import tensorflow as tf
 import os.path
 from stardist.models import StarDist2D
 import matplotlib
-matplotlib.use('agg')
-from stardist import random_label_cmap
 from Slideshow import Slideshow
 import Settings
 import sys
@@ -14,6 +12,8 @@ from SettingsWindow import SettingsWindow
 from Predictions import Predictions
 from ExcelWindow import ExcelWindow
 import Utilities as utils
+matplotlib.use('agg')
+
 
 def resource_path(relative_path):
     """ Get absolute path to resource, needed for PyInstaller """
@@ -43,20 +43,43 @@ Methods:
     help page: creates a help page pop up for the users
 '''
 class MainFrame(ttk.Frame):
-    '''
-    method: Initialization method
-    Authors: Alex Mensen-Johnson, Skylar Wilson
-    '''
+    """
+    Class Main Frame
+    Author: Max
+    Contributors: Skylar Wilson, Alex Mensen-Johnson, Sunella Ramnath
+    Class: Main Frame
+    Description: Main Frame of GUI, all sub frames will be loaded inside of this class
+    Params:
+        container: a container containing the title and geometry of the Graphic User Interface
+    Methods:
+        init: Initialization method
+        create display: method to create the buttons and subframes of the main frame
+        load display: method to load data into the GUI
+        select files: method for selecting files to be predicted on
+        select model: method for selecting the model for prediction
+        predict: method for predicting egg count for the frog eggs. outputs the photos saved with the number count
+        predict all: predicts the values of all the loaded files in the GUI
+        clear images: Clears the images from the GUI
+        help page: creates a help page pop up for the users
+    """
     def __init__(self, container):
+        """
+        method: init
+        description: Initialization method
+        :param container:
+        """
+        # initialize the container's parant init methods
         super().__init__(container)
+        # initialize the settings
         self.settings = Settings.SettingsJson()
+        # initialize the container method
         self.container = container
+        # create the image files list, set to empty
         self.image_files = []
+        # create the prediciton files, leave them empty
         self.prediction_files = {}
         # initialize predictions_data list -skylar
         self.predictions_data = []
-        # initialize lbl_cmap for random color map -skylar
-        self.lbl_cmap = random_label_cmap()
         # variable for the model
         self.model = None
         # initialize the excel editor class
@@ -71,12 +94,13 @@ class MainFrame(ttk.Frame):
         self.is_excel_save_page_open = False
         # boolean checker to see if the settings window is open
         self.is_settings_page_open = False
+        # set the device's GPU
         self.device = '/GPU:0' if tf.config.experimental.list_physical_devices('GPU') else '/CPU:0'
-        self.progress_popup = None  # Initialize progress_popup to None
+        # initialize the progress popup to None
+        self.progress_popup = None
+        # set the model path to nothing
         self.model_path = ''
-
         # settings for automatic excel export
-        # i made it so it can return a boolean value
         self.automatic_excel_setting = self.settings.get_automatic_excel_export()
         # print(self.automatic_excel_setting)
         self.predict_index = 1
@@ -99,12 +123,12 @@ class MainFrame(ttk.Frame):
         # empty variable for excel window
         self.excel_window = None
 
-    '''
-    Author: Alex Mensen-Johnson
-    Creates the display in the code and organizes all the creation into a method
-    If you intend on creating a feature for display, add it here please
-    '''
     def create_display(self):
+        """
+        Creates the display in the code and organizes all the creation into a method
+        If you intend on creating a feature for display, add it here please
+        :return:
+        """
         # Creates the select files button
         self.select_files_button = ttk.Button(self, text='Select Files', command=self.select_files)
         # Creates the slide show
@@ -128,13 +152,12 @@ class MainFrame(ttk.Frame):
         # Create settings page
         self.settings_page_button = ttk.Button(self, text='Settings', command=lambda: self.open_settings_window())
 
-
-    '''
-    method: button_dict
-    Author: Alex Mensen-Johnson
-    returns a dictionary of all the buttons for the creation of tooltips
-    '''
     def button_dict(self):
+        """
+        method: button_dict
+        Author: Alex Mensen-Johnson
+        :return: a dictionary of all the buttons for the creation of tooltips
+        """
         return {
             'select_model': self.select_model_button,
             'select_files': self.select_files_button,
@@ -145,12 +168,14 @@ class MainFrame(ttk.Frame):
             'settings_window': self.settings_page_button
         }
 
-    '''
-    Author: Alex Mensen-Johnson
-    organizes the display buttons into a method for loading the display in a clear format
-    If you are loading anything into the display, please add it here.
-    '''
     def load_display(self):
+        """
+        method: load_display
+        Author: Alex Mensen-Johnson
+        organizes the display buttons into a method for loading the display in a clear format
+        If you are loading anything into the display, please add it here.
+        :return:
+        """
         # using grid layout -skylar
         self.grid_rowconfigure(0, weight=0)
         # using grid layout -skylar
@@ -175,62 +200,115 @@ class MainFrame(ttk.Frame):
         self.excel_window_button.grid(row=9, column=0, pady=5)
         # adds the settings button to the window
         self.settings_page_button.grid(row=10,column=0,pady=5)
-        # Add the progress bar and labels to the window -skylar
-        # self.progress_bar.grid(row=11, column=0, pady=5)
-        # self.predicted_images_label.grid(row=12, column=0, pady=5)
-        # self.estimated_time_label.grid(row=13, column=0, pady=5)
 
-    '''****************************************************************************************'''
-    '''Destroys current frame and reloads MainFrame for the purpose of dynamic display. -skylar'''
     def update_display(self):
+        """
+        method: update_display
+        Author: Skylar Wilson
+        Destroys current frame and reloads MainFrame for the purpose of dynamic display.
+        :return:
+        """
+        # make the parent container the same container
         parent_container = self.container
+        # destroy the mainframe window
         self.destroy()
-        
         # Re-initialize the frame with the same container
         new_frame = MainFrame(parent_container)
-
         # Add the new frame to the container using grid
         new_frame.grid(row=0, column=0, sticky='nsew')
 
     def disable_button(self, button):
-        button.config(state = tk.DISABLED)
+        """
+        method: disable_button
+        Author: Skylar Wilson
+        :param button: the button that will be disabled
+        """
+        # Disable the button
+        button.config(state=tk.DISABLED)
 
     def enable_button(self, button):
-        button.config(state = tk.NORMAL)
+        """
+        method: enable_button
+        Author: Skylar Wilson
+        :param button: the button that will be enabled
+        """
+        # Enables the button
+        button.config(state=tk.NORMAL)
 
     def select_files(self):
+        """
+        method: select_files
+        Author: Max, Alex Mensen-Johnson
+        allows the user to manually select files to be predicted
+        :return:
+        """
+        # opens the dialog box for the user to select files
         files = filedialog.askopenfilenames(initialdir='/home/max/development/stardist/data')
+        # initialize an empty list to hold the files
         file_list = []
+        # loop through the selected files
         for image in files:
-            print(f'This is the image file {image}')
+            # split the file names by a period to check the file extension
             file_name_split = image.split('.')
+            # check if the file is a .tif, .tiff, .png, .jpg, .jpeg, .gif, .bmp, .npy, .npz, or .heic
             if file_name_split[-1] != 'tif' and file_name_split[-1] != 'tiff' and file_name_split[-1] != 'png' and file_name_split[-1] != 'jpg' and file_name_split[-1] != 'jpeg' and file_name_split[-1] != 'gif' and file_name_split[-1] != 'bmp' and file_name_split[-1] != 'npy' and file_name_split[-1] != 'npz' and file_name_split[-1] != 'heic':
+                # create a warning if the file is not compatible
                 answer = messagebox.askyesno('Incompatible File Type Warning', f'A selected file is not a .tif, .tiff, .png, .jpg, .jpeg, .gif, .bmp, .npy, .npz, or .heic file.\n Would you like to remove the file and continue?\n Select no to abort the action\n your file type is {file_name_split[-1]}')
+                # if the user does not want to continue, move to the next iteration of the loop
                 if answer == True:
-                    break
+                    # move to the next iteration of the loop
+                    continue
                 else:
+                    # leave method
                     return
+            # append the image to the list
             file_list.append(image)
+        # add files to the image files variables
         self.image_files.extend(file_list)
+        # update the predictions image files
         self.predictions.image_files = self.image_files
+        # if the files exist
         if self.image_files:
+            # update the slideshow
             self.slideshow.update_image()
 
     def select_model(self):
+        """
+        method: select_model
+        Author: Skylar Wilson, Max
+        allows the user to manually select a model
+        :return:
+        """
+        # update the model path variable with a user selected model
         self.model_path = filedialog.askdirectory()
+        # print the model path
         print(self.model_path)
+        # print the full file name of the model path
         print(os.path.basename(self.model_path))
+        # print the directory of the model path
         print(os.path.dirname(self.model_path))
+        # open with tensor flow
         with tf.device(self.device):
+            # set the model to the stardist model
             self.model = StarDist2D(None, name=os.path.basename(self.model_path), basedir=os.path.dirname(self.model_path))
+        # change prediction model to the current model
         self.predictions.model = self.model
+        # configure the label to display the model
         self.model_label.config(text=f'Current Model: {os.path.basename(self.model_path)}')
+        # if the model is selected, then enable the button
         if not self.model_selected:
+            # enable the predict all button
             self.predict_all_button.config(state=tk.NORMAL)
+            # ste the model selected to true
             self.model_selected = True
 
 
     def clear_images(self):
+        """
+        method: clear_images
+        Author:Alex Mensen-Johnson
+        clears the images from the GUI
+        """
         # Set the base image reference to None
         self.slideshow.base_image.image_ref = None
         # Clear the label of the file path above the slideshow
@@ -254,20 +332,21 @@ class MainFrame(ttk.Frame):
         self.prediction_files = {}
         # sets the slideshow prediction files to empty
         self.slideshow.prediction_files = self.prediction_files
-
+        # reset the slideshow index
         self.slideshow.current_index = 0
-
-        if self.settings.get_clear_data_on_clear_images():
-            self.predictions.predictions_data.clear()
-            self.predictions_data.clear()
-            self.is_data_cleared = True
-        #
+        # display a message box showing the Images are cleared
         messagebox.showinfo("Devision", "Images Cleared!")
-    '''
-    Help Page: by Alex Mensen-Johnson
-    Description: Loads the Help_Information.txt into a file, reads the file, and displays the information in a pop up
-    '''
+        # check the settings variable
+        if self.settings.get_clear_data_on_clear_images():
+            # clear the data
+            self.clear_predicted_data()
+
+
     def help_page(self):
+        """
+        Help Page: by Alex Mensen-Johnson
+        Description: Loads the Help_Information.txt into a file, reads the file, and displays the information in a pop up
+        """
         # Load File
         info_file = open(resource_path("docs/Help_Information.txt"))
         # Read the file
@@ -278,24 +357,33 @@ class MainFrame(ttk.Frame):
         messagebox.showinfo(title,file_information)
 
     def clear_predicted_data(self):
+        """
+        clear the predicted data
+        :return: no return
+        """
+        # clear the predictions data
         self.predictions.predictions_data.clear()
+        # set the is data cleared variable to True
         self.is_data_cleared = True
+        # display a message box showing the data is cleared
+        messagebox.showinfo("Devision", "Data Cleared")
 
-
-    '''
-    function to open the excel window
-    '''
     def open_excel_window(self):
+        """
+        method: opens the excel window if an instance is not already open
+        :return:
+        """
         # check if the excel window is already open
         if self.is_excel_save_page_open == False:
             # open the excel window
             self.excel_window = ExcelWindow(master=self, container=self.container, excel_editor=self.excel_editor)
 
-    '''
-    method: open settings window
-    description: opens the settings window if it is not already open
-    '''
     def open_settings_window(self):
+        """
+        method: open_settings_window
+        description: opens the settings window if it is not already open
+        :return:
+        """
         # check if the settings window is already open
         if self.is_settings_page_open == False:
             # open the settings window
