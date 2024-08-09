4bd51fe7038dd8bfd1ad5b486d844215db605142
alexmj003 <idkyflymf@gmail.com>
2024-08-09 09:27:16 -0500

    added None to the list of none saveable filenames

--- Utilities.py
+++ Utilities.py
@@ -143,7 +143,7 @@ class StringChecker():
         self.files_cannot_end_with = list of files that cannot end with a period
         """
         self.invalid_characters = ['\\', '/', ':', ';', '?', '*', '<', '>', '|', '"', '.']
-        self.invalid_filenames = ['CON', 'PRN', 'AUX', 'NUL', 'COM0', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9', 'LPT0', 'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9']
+        self.invalid_filenames = ['CON', 'PRN', 'AUX', 'NUL', 'COM0', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9', 'LPT0', 'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9','NONE']
         self.files_cannot_end_with = ['.', ' ']
 
     def filename_checker(self, filename):
