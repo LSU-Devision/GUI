87b21632f90c64845e1ce073f889602331f2eb60
alexmj003 <idkyflymf@gmail.com>
2024-08-12 11:32:33 -0500

    added sections to the default settings json. updated methods to reflect changes, no changes to front end yet

--- Settings.py
+++ Settings.py
@@ -24,6 +24,8 @@ class SettingsJson():
         with open(json_file_path, 'r') as default_json_file:
             self.default_json_data = json.load(default_json_file)
         self.json_file = self.default_json_data
+        self.runtime_json_data = self.default_json_data['runtime settings']
+        self.saved_files_json_data = self.default_json_data['saved files']
         self.load_default()
 
 
@@ -33,10 +35,10 @@ class SettingsJson():
         :description: Loads the default settings
         :return: nothing
         """
-        self.automatic_excel_export = self.default_json_data['automatic excel export']
-        self.automatic_prediction_clear_data = self.default_json_data['automatic prediction clear data']
-        self.clear_data_on_clear_images = self.default_json_data['clear data on clear images']
-        self.save_images_output = self.default_json_data['save images output']
+        self.automatic_excel_export = self.runtime_json_data['automatic excel export']
+        self.automatic_prediction_clear_data = self.runtime_json_data['automatic prediction clear data']
+        self.clear_data_on_clear_images = self.runtime_json_data['clear data on clear images']
+        self.save_images_output = self.runtime_json_data['save images output']
 
 
     def revert_to_default(self):
@@ -49,10 +51,11 @@ class SettingsJson():
         self.automatic_prediction_clear_data = False
         self.clear_data_on_clear_images = True
         self.save_images_output = False
-        self.default_json_data['automatic excel export'] = self.automatic_excel_export
-        self.default_json_data['automatic prediction clear data'] = self.automatic_prediction_clear_data
-        self.default_json_data['clear data on clear images'] = self.clear_data_on_clear_images
-        self.default_json_data['save images output'] = self.save_images_output
+        self.runtime_json_data['automatic excel export'] = self.automatic_excel_export
+        self.runtime_json_data['automatic prediction clear data'] = self.automatic_prediction_clear_data
+        self.runtime_json_data['clear data on clear images'] = self.clear_data_on_clear_images
+        self.runtime_json_data['save images output'] = self.save_images_output
+        self.default_json_data['runtime settings'] = self.runtime_json_data
 
         with open(utils.resource_path(default_json_path), 'w') as json_file:
             json.dump(self.default_json_data, json_file, indent=4)
@@ -60,7 +63,7 @@ class SettingsJson():
         self.load_default()
         
 
-    def update_json(self, key, value):
+    def update_json(self, key, value,section='runtime settings'):
         """
         :method: update_json
         :description: Updates the json file with new settings
@@ -70,7 +73,13 @@ class SettingsJson():
         """
 
         # Update data
-        self.default_json_data[key] = value
+        self.runtime_json_data[key] = value
+        if section == 'runtime settings':
+            self.runtime_json_data[key] = value
+            self.default_json_data['runtime settings'] = self.runtime_json_data
+        elif section == 'saved files':
+            self.saved_files_json_data[key] = value
+            self.default_json_data['saved files'] = self.saved_files_json_data
 
         # Overwrite all settings in default-settings.json with updated data
         with open(utils.resource_path(default_json_path), 'w') as json_file:
@@ -94,16 +103,16 @@ class SettingsJson():
     # each set function uses boolean input and writes as boolean -skylar
     def set_automatic_excel_export(self, value):
         self.automatic_excel_export = value
-        self.update_json('automatic excel export', value)
+        self.update_json('automatic excel export', value, 'runtime settings')
 
     def set_automatic_prediction_clear_data(self, value):
         self.automatic_prediction_clear_data = value
-        self.update_json('automatic prediction clear data', value)
+        self.update_json('automatic prediction clear data', value, 'runtime settings')
 
     def set_clear_data_on_clear_images(self, value):
         self.clear_data_on_clear_images = value
-        self.update_json('clear data on clear images', value)
+        self.update_json('clear data on clear images', value, 'runtime settings')
 
     def set_save_images_output(self, value):
         self.save_images_output = value
-        self.update_json('save images output', value)
+        self.update_json('save images output', value, 'runtime settings')
