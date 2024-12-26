f823c32f555d422c2008861005e3fa483cedf083
dhdschool <dhdschool@outlook.com>
2024-12-26 12:27:55 -0600

    Update requirments.txt, add scikit-learn, begin columns in OysterExcel.py

--- src/OysterExcel.py
+++ src/OysterExcel.py
@@ -2,9 +2,14 @@ import pandas as pd
 from src.SettingsWindowProposal import SettingsWindow
 
 class OysterExcel():
-    def __init__(self, *args):
-        # Inherit settings object from parent, this will fail if it is not passed a valid settings object
+    def __init__(self, *args, **kwargs):
+        # Inherit settings object from parent, this will fail if it is not passed a valid parent object (typically
+        # mainframe or one of mainframe's children)
         self.settings = args[0].settings
+
+        # Mass is in grams unless otherwise specified
+        self.df = pd.DataFrame(columns=['size-class', 'seed-tray-weight', 'slide-weight', 'slide-and-seed-weight', 'subsample-count', 'total-number', ])
+        
         
         
   
