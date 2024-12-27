3824d35b5ea63503cb892f4609ac1396bc6852f9
dhdschool <dhdschool@outlook.com>
2024-12-26 20:45:07 -0600

    Delete misnamed warningwindow

--- src/WarningWindow.py
+++ src/WarningWindow.py
@@ -1,2 +1,14 @@
 import tkinter as tk
 import tkinter.ttk as tk
+
+class WarningWindow(tk.Toplevel):
+    def __init__(self, root):
+        self.width = root.winfo_screenwidth()
+        
+
+if __name__ == '__main__':
+    print('here')
+    root = tk.Tk()
+    new_window = WarningWindow(root)
+    print(new_window.width)
+    root.mainloop()
